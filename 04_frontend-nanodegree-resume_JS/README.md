d4326c02f5a72cca7ce0b70745ec958e3789b348
Nicolepcx <nicolekoenigstein@me.com>
2016-06-19 13:12:10 +0200

    Projectwork
    Modified JS Project

---
+++ 04_frontend-nanodegree-resume_JS/README.md
@@ -0,0 +1,18 @@
+<h1>Resume</h1>
+<p>resume project for Udacity Front End Web Development Nanodegree</p>
+<p><a href="http://nicolekoenigstein.com/Nicole/04_frontend-nanodegree-resume/index.html">Demo</a></p>
+
+<h1>References</h1>
+<ul>
+  <li><a href="http://www.chartjs.org/docs/#polar-area-chart">Chart.js polar area chart library</li>
+  <li><a href="http://www.brightpointinc.com/clients/brightpointinc.com/library/radialProgress/index.html?source=d3js">D3 Radial Progress Component</li>
+  <li><a href="https://icomoon.io/app/#/select">Iconmoon</a> - Icon generation app</li>
+  <li><a href="http://getbootstrap.com/">Bootstrap</a> - CSS framework</li>
+  <li><a href="https://udacity.github.io/frontend-nanodegree-styleguide/">Udacity HTML/CSS style guide</li>
+  <li>Udacity JS helper</li>
+  <li><a href="https://developers.google.com/maps/documentation/javascript/tutorial">Google Maps API</a></li>
+  <li><a href="http://validator.w3.org/">W3C HTML Validator</a></li>
+  <li><a href="http://jigsaw.w3.org/css-validator/">W3C CSS Validator</a></li>
+  <li><a href="https://github.com/codrops/AnimatedBorderMenus/blob/master/css/icons.css">Codrops Animated Border Menu</a> - Checked icons.css Codrops Animated Border Menu tutorial for reference</li>
+  <li><a href="http://weloveiconfonts.com/">WE LOVE ICON FONTS</a></li>
+</ul>
---
+++ 04_frontend-nanodegree-resume_JS/js/resumeBuilder.js
@@ -0,0 +1,275 @@
+
+//Work JSON
+var work = {
+    "jobs":[
+        {
+            "employer": "Medienbotschaft Verlag & Events GmbH",
+            "title": "Creative Director",
+            "location": "Tägerwilen, TG, Switzerland",
+            "dates": "2014 - Present",
+            "description": "Coordination of the online and offline projects of the publishing house. I oversee the creative process and give guidance to the creative people that work under me.  "
+        },
+        {
+            "employer": "Medienbotschaft Verlag & Events GmbH",
+            "title": "Senior Editorial Designer",
+            "location": "Tägerwilen, TG, Switzerland",
+            "dates": "2009 - 2014",
+            "description": "Coordination of the online and offline projects of the publishing house."
+        },
+        {
+            "employer": "Medienbotschaft Verlag & Events GmbH",
+            "title": "Marketing|Event-Manager",
+            "location": "Tägerwilen, TG, Switzerland",
+            "dates": "2007 - 2009",
+            "description": "Coordination Marketing and Events."
+        }
+
+    ]
+
+};
+// Display work json info
+work.display = function(){
+
+    for (var job in work.jobs){
+        $("#workExperience").append(HTMLworkStart);
+        $(".work-entry:last").append(HTMLworkTitle.replace("%data%", work.jobs[job]["title"]))
+                                .append(HTMLworkEmployer.replace("%data%", work.jobs[job]["employer"]))
+                                .append(HTMLworkLocation.replace("%data%", work.jobs[job]["location"]))
+                                .append(HTMLworkDates.replace("%data%", work.jobs[job]["dates"]))
+                                .append(HTMLworkDescription.replace("%data%", work.jobs[job]["description"]));
+    }
+
+}
+// Projects JSON
+var projects = {
+    "projects": [
+        {
+            "title": "Resume",
+            "date worked": "2016",
+            "description": "Resume project for Udacity Front-end Web Development Nanodegree",
+            "image": "images/Print_Screen_Portfolio.png",
+            "github": "https://github.com/Nicolepcx/00_Portfolio_Website_NK/tree/master",
+            "demo": "http://nicolepcx.github.io/",
+        }
+
+    ]
+
+};
+// Display project json info
+projects.display = function(){
+    var d3Div = new Array(projects.projects.length);
+    var progressDiv = new Array(projects.projects.length);
+
+    for (var project in projects.projects){
+
+            $("#projects").append(HTMLprojectStart);
+
+            $(".project-entry:last").append('<div id="div' + project + '"></div>')
+                                        .append(HTMLprojectTitle.replace("%data%", projects.projects[project]["title"]))
+                                        .append(HTMLprojectImage.replace("%data%", projects.projects[project]["image"]))
+                                        .append(HTMLprojectDates.replace("%data%", projects.projects[project]["date worked"]))
+                                        .append(HTMLprojectDescription.replace("%data%", projects.projects[project]["description"]));
+            // display demo or github json info if they exisit
+            if (projects.projects[project]["demo"] != undefined )
+                $(".project-entry:last").append(HTMLprojectDemo.replace("#", projects.projects[project]["demo"]));
+            if (projects.projects[project]["github"] != undefined )
+                $(".project-entry:last").append(HTMLprojectGithub.replace("#", projects.projects[project]["github"]));
+
+    }
+
+};
+
+//Bio JSON
+var bio = {
+    "first name": "Nicole",
+    "last name": "Königstein",
+    "role": "Front-End Developer",
+    "welcome message": "–Welcome to my Resume Website!– \n I am currently working as a Creative Director at a publishing house. I love to be creative @ work, but I'm also passionate about Programming, Data Science, Machine Learning, Web Design and Web Development. Therefore, I am looking forward to studying more in my foreseeable future, to gain more experience and expertise.",
+    "bio pic": "images/Passfoto_Nicole_Koenigstein.jpg",
+    "contacts":
+        {
+            "mobile number": "+41-78-834 12 15",
+            "email": "nicolepcx@gmail.com",
+            "github username": "Nicolepcx",
+            "twitter": "nickoenigstein",
+            "location": "Kreuzlingen, TG"
+        },
+    "skills": ["Adobe Creative Suite", "Grafik & Editorial-Design","HTML", "CSS", "Javascript", "JQuery", "WordPress", "Bootstrap"]
+
+};
+bio.display = function(){
+    var formattedFName = HTMLheaderFName.replace("%data%", bio["first name"].toUpperCase());
+    var formattedLName = HTMLheaderLName.replace("%data%", bio["last name"].toUpperCase());
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role.toUpperCase());
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile number"]);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github username"]);
+    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio["bio pic"]);
+    var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio["welcome message"]);
+
+
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedLName);
+    $("#header").prepend(formattedFName);
+    $("#header").append(formattedWelcomeMsg);
+    $("#topContacts ul").append(formattedMobile);
+    $("#topContacts ul").append(formattedEmail);
+    $("#topContacts ul").append(formattedGithub);
+    $("#topContacts ul").append(formattedTwitter);
+    $("#topContacts ul").append(formattedLocation);
+
+}
+//Education JSON
+var education = {
+    "schools": [
+        {
+            "name": "Hamburger Technische Kunstschule",
+            "location": "Hamburg, DE",
+            "degree": "Bachlor of Arts",
+            "majors": ["Editorial Design"],
+            "dates": "2007 - 2009",
+            "url": "http://www.htk-ak.de/"
+        },
+    ],
+    "onlineCourses": [
+        {
+            "title": "Passion4digital, Social Media",
+            "school": "iversity",
+            "dates": 2015,
+            "url": "https://iversity.org/"
+        },
+        {
+            "title": "Intro to Programming Nanodegree",
+            "school": "Udacity",
+            "dates": 2016,
+            "url": "https://www.udacity.com/nanodegrees"
+        },
+        {
+            "title": "Front-end Web Development",
+            "school": "Udacity",
+            "dates": 2016,
+            "url": "https://www.udacity.com/nanodegrees"
+        }
+    ],
+};
+// Display education json info
+education.display = function(){
+    for (var school in education.schools){
+        $("#education").append(HTMLschoolStart);
+        $(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[school]["dates"]))
+                                    .append(HTMLschoolName.replace("%data%", education.schools[school]["name"]).replace("#", education.schools[school]["url"]))
+                                    .append(HTMLschoolLocation.replace("%data%", education.schools[school]["location"]));
+
+        for(var major in education.schools[school].majors){
+            $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[school]["majors"][major]));
+        }
+    }
+    $("#education").append(HTMLonlineClasses);
+    // Display online courses
+    for (var onlineCourse in education.onlineCourses){
+        var num = education.schools.length + parseInt(onlineCourse);
+        $("#education").append(HTMLschoolStart);
+        $(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse]["dates"]))
+                                    .append(HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse]["school"]))
+                                    .append(HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse]["title"]))
+                                    .append(HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse]["url"]));
+    }
+}
+//Map
+$("#mapDiv").append(googleMap);
+
+/// chartjs library from: http://www.chartjs.org/docs/#polar-area-chart
+var polarData = [
+  {
+    value: 10,
+    color:"#F7464A",
+    highlight: "#FF5A5E",
+    label: "Adobe Creative Suite",
+    labelcolor: "red"
+  },
+  {
+    value: 10,
+    color:"#F06613",
+    highlight: "#FF5A5E",
+    label: "Grafik & Editorial-Design",
+    labelcolor: "orange"
+  },
+  {
+    value: 8,
+    color: "#46BFBD",
+    highlight: "#5AD3D1",
+    label: "HTML",
+    labelcolor: "green"
+  },
+  {
+    value: 8,
+    color: "#FDB45C",
+    highlight: "#FFC870",
+    label: "CSS",
+    labelcolor: "yellow"
+  },
+   {
+    value: 8,
+    color: "#032239",
+    highlight: "#0a5b97",
+    label: "WordPress",
+    labelcolor: "darker-blue"
+  },
+  {
+    value: 5,
+    color: "#949FB1",
+    highlight: "#A8B3C5",
+    label: "JavaScript",
+    labelcolor: "gray"
+  },
+
+   {
+    value: 5,
+    color: "#23582b",
+    highlight: "#06cc24",
+    label: "Python",
+    labelcolor: "darker-green"
+  },
+  {
+    value: 3,
+    color: "#4D5360",
+    highlight: "#616774",
+    label: "MySQL",
+    labelcolor: "darker-gray"
+  }
+
+  ];
+
+// Display a list of skill labels
+var skillsChartLabels = function(){
+    for (skill in polarData){
+      var skillLabel = polarData[skill].label;
+      var skillLabelColor = polarData[skill].labelcolor;
+      var skillHTML = '<span class="label ' + skillLabelColor + '">' + skillLabel + '</span>';
+      $("#skills-list").append(skillHTML);
+    }
+}
+
+
+// Call functions
+window.onload = function(){
+    var ctx = document.getElementById("skills-chart").getContext("2d");
+    window.myPolarArea = new Chart(ctx).PolarArea(polarData, {
+        responsive:false
+    });
+    // Call skillsChartLabels function defined
+    skillsChartLabels();
+};
+// Call bio, work, projects and education functions
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+
+
+
+
+
